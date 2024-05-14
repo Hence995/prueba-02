929748ce3e3a5c2ecef1aa517f8622a1a94600ee
Hence995 <figueroasebastian1@gmail.com>
2024-05-14 20:30:23 -0300

    repaso02

--- src/repaso/Repaso.java
+++ src/repaso/Repaso.java
@@ -20,20 +20,28 @@ public class Repaso {
         
         Scanner leer = new Scanner(System.in);
         
-        System.out.println("Ingrese su nombre: ");
-        String nombre = leer.nextLine();
-        System.out.println("Mi nombre es " +nombre);
+        //System.out.println("Ingrese su nombre: ");
+        //String nombre = leer.nextLine();
+        //System.out.println("Mi nombre es " +nombre);
         
-        System.out.println("Ingrese su edad: ");
-        int edad = leer.nextInt();
-        System.out.println("Mi edad es " +edad);
+        //System.out.println("Ingrese su edad: ");
+        //int edad = leer.nextInt();
+        //System.out.println("Mi edad es " +edad);
        
-        System.out.println("Ingrese su correo; ");
-        String correo = leer.next();
-        System.out.println("Mi correo es "+ correo);
+        //System.out.println("Ingrese su correo; ");
+        //String correo = leer.next();
+        //System.out.println("Mi correo es "+ correo);
         
-        System.out.printf("Mi nombre es %s , mi edad es %d y mi correo es %s");
-         // TODO code application logic here
+        // CONDICIONALES
+        // SON CONDICIONES SI PASA X = ALGO SI NO = OTRA COSA
+        // IF, IF ELSE, ELSE
+        int numx = 1;                   
+        if (numx > 1) {
+            System.out.println("Es verdad");   
+        } else {
+            System.out.println("Es falso");
+        }
+        // TODO code application logic here
     }
     
 }
